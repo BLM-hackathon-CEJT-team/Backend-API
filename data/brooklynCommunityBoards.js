1d604ecf54d6728ee7b68537708e0997bc448990
jacquelineturof <jacquelineturof@gmail.com>
2020-06-13 14:34:58 -0400

    brooklyn community boards added in

---
+++ data/brooklynCommunityBoards.js
@@ -0,0 +1,94 @@
+const brooklynCommunityBoards = [
+    {
+        district: 34,
+        email: "bk01@cb.nyc.gov",
+        zip: 11211 
+    },
+    {
+        district: 33,
+        email: "cb2k@nyc.rr.com",
+        zip: 11201 
+    },
+    {
+        district: 36,
+        email: "bk03@cb.nyc.gov",
+        zip: 11216 
+    },
+    {
+        district: 37,
+        email: "BK04@cb.nyc.gov",
+        zip: 11207
+    },
+    {
+        district: 42,
+        email: "bk05@cb.ny.gov",
+        zip: 11208
+    },
+    {
+        district: 39,
+        email: "info@Brooklyn.CB6.org",
+        zip: 11201
+    },
+    {
+        district: 38,
+        email: "bk07@cb.nyc.gov",
+        zip: 11232
+    },
+    {
+        district: 36,
+        email: "info@brooklyncb8.org",
+        zip: 11213
+    },
+    {
+        district: 35,
+        email: "bk09@cb.nyc.gov",
+        zip: 11225
+    },
+    {
+        district: 43,
+        email: "bk10@cb.nyc.gov",
+        zip: 11209
+    },
+    {
+        district: 47,
+        email: "bk11@cb.nyc.gov",
+        zip: 11214
+    },
+    {
+        district: 44,
+        email: "bk12@cb.nyc.gov",
+        zip: 11219
+    },
+    {
+        district: 47,
+        email: "Info@bk13@cb.nyc.gov",
+        zip: 11224
+    },
+    {
+        district: 45,
+        email: "Info@cb14brooklyn.com",
+        zip: 11239
+    },
+    {
+        district: 48,
+        email: "bklcb15@verizon.net",
+        zip: 11235
+    },
+    {
+        district: 41,
+        email: "bk16@cb.nyc.gov",
+        zip: 11212
+    },
+    {
+        district: 45,
+        email: "bk17@cb.nyc.gov",
+        zip: 11210
+    },
+    {
+        district: 46,
+        email: "bkbrd18@optonline.net",
+        zip: 11234
+    },
+]
+
+module.exports = { brooklynCommunityBoards }
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const axios = require("axios")
+
+// DATA SETS
 const { cityCouncil } = require("./data/cityCouncilMembers")
+const { brooklynCommunityBoards } = require("./data/brooklynCommunityBoards")
 
 const app = express()
 
@@ -11,18 +14,46 @@ const app = express()
 */
 app.get("/api/info/:address", async (req, res) => {
     const address = req.params.address
+    console.log(address)
     const formattedAddress = address.replace(/ /g,"%20");
 
     const key = process.env.GOOGLE_API_KEY
     try {
         const url = `https://www.googleapis.com/civicinfo/v2/representatives?key=${key}&address=${formattedAddress}`
         const response = await axios.get(url)
+        let zip
+
+        // Need zip to get borough board members
+        // throw error if we don't have it
+        if (response.data.normalizedInput.zip) {
+            zip = response.data.normalizedInput.zip
+        } else {
+            throw new Error("Please include your zip code.")
+        }
+        
         const borough = response.data.normalizedInput.city
         const councilMembers = cityCouncil.filter(member => member.borough === borough)
+        let communityBoard
+
+        // Get borough board members
+        if (borough === "Brooklyn") {
+
+            communityBoard = brooklynCommunityBoards.find(board => {
+                console.log(typeof parseInt(board.zip))
+                console.log(typeof zip)
+                return parseInt(board.zip) == zip
+            })
+
+            // Make sure we found board members, check for undefined
+            if (communityBoard === undefined) {
+                communityBoard = "Board Members Not Found."
+            }
+        }
+
         // Send back the offices and the council members
-        res.send({ offices: response.data.offices, councilMembers })
+        res.send({ offices: response.data.offices, councilMembers, communityBoard })
     } catch (e) {
-        res.status(500).send("Incorrect message format. Please add city and state!")
+        res.status(500).send("Incorrect message format. Please provide full address")
     }
 })
 
